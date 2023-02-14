bbb80855e5469e8fe83664e8b15fff77429a6201
Andrey Fedorov <kingzor@icloud.com>
2023-02-14 21:23:35 +0300

    add HomeWork_Lesson5

---
+++ HomeWork_Lesson5/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] massive = { 3, 7, 23, 12 };
+
+int result = 0;
+for (int i = 0; i < massive.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        result = result + massive[i];
+    }
+}
+System.Console.WriteLine(result);
---
+++ HomeWork_Lesson5/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] massive = { 3, 7, 22, 2, 78 };
+
+int min = int.MaxValue;
+int max = int.MinValue;
+for (int i = 0; i < massive.Length; i++)
+{
+    if (min > i) min = massive[i];
+    if (max < i) max = massive[i];
+}
+int result = max - min;
+System.Console.WriteLine(result);
