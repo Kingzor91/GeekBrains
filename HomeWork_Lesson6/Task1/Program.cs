ee81ab76fb00dfd3d491ec862b631b31c3a7da8d
Andrey Fedorov <kingzor@icloud.com>
2023-02-15 18:11:36 +0300

    add HomeWork_Lesson6

---
+++ HomeWork_Lesson6/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+string Input(string message)    // Параметр - подсказка пользователю
+{
+    Console.Write(message + " > ");             // Вывод сообщения пользователю
+    string? inputValue = Console.ReadLine();    // Ввод строки пользователем
+    return inputValue;                          // возврат результата
+}
+
+List<int> ToList(string message)
+{
+    string[] s = message.Split(",");
+    List<int> list = new List<int>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        list.Add(Convert.ToInt32(s[i]));
+    }
+    return list;
+}
+
+int Count(List<int> list)
+{
+    int count = 0;
+    for (int i = 0; i < list.Count; i++)
+    {
+        if (list[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+List<int> list = ToList(Input("Введите с клавиатуры М числе разделенных запятой."));
+System.Console.WriteLine(Count(list));
---
+++ HomeWork_Lesson6/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double Input(string message)    // Параметр - подсказка пользователю
+{
+    Console.Write(message + " > ");             // Вывод сообщения пользователю
+    double inputValue = Convert.ToDouble(Console.ReadLine());    // Ввод строки пользователем
+    return inputValue;                          // возврат результата
+}
+
+Tuple<double, double> Function()
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+    return Tuple.Create(x,y);
+}
+
+double b1 = Input("b1");
+double k1 = Input("k1");
+double b2 = Input("b2");
+double k2 = Input("k2");
+
+Tuple<double, double> result = Function();
+System.Console.WriteLine($"({result.Item1}; {result.Item2})");
